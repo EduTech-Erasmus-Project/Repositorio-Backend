2bf8238f48022d488963529ad816b5fbd8c13f57
Angel <angelpaqui36@gmail.com>
2021-11-05 00:48:31 -0500

    token message

--- roabackend/settings.py
+++ roabackend/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'l=7$@=!muo8kj*j+6nq!wl2n$8pn*m^kza*=g60g536#rtb%__'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS=[]
 if DEBUG:
